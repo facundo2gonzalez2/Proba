c7a11ef00acaaed8c18deb55d64d89e2dd722d1e
facundo2gonzalez2 <facundo2gonzalez2@gmail.com>
2022-05-26 11:41:56 -0300

    practica 6

---
+++ Practica6/practica6.r
@@ -0,0 +1,55 @@
+#PRACTICA 6
+
+#ejercicio 1
+data=read.table(file= "alfajores.txt" ,header=TRUE)
+n=500
+
+#a
+proba_quilmes = sum(data$fabrica==0)/n
+proba_quilmes
+
+#b
+proba_quilmesy3defec = sum(data$defectuosos==3 & data$fabrica==0)/n
+proba_quilmesy3defec
+
+#c
+pxy = t(table(data)) / n
+pxy
+
+#d
+mean(data$defectuosos)
+
+var(data$defectuosos)
+
+#e
+#P(X=3|Y=0)= Pxy(3,0)/Py(0)
+pxy[1,4] / sum(pxy[1,])
+
+#f
+# Px(3)
+sum(pxy[,4])
+
+#g
+#P(Y=0|X=3) = Pxy(0,3) / Px(3)
+pxy[1,4] / sum(pxy[,4])
+
+#h
+#P(Y=1|X=3) = Pxy(1,3) / Px(3)
+pxy[2,4] / sum(pxy[,4])
+
+
+#ejercicio 2
+lamparas=read.table(file="lamparas.txt", header = FALSE)
+cant_lamparas=26
+
+#a
+
+proba_mas30=sum(lamparas>30)/cant_lamparas
+proba_mas30
+
+#b
+
+
+
+
+

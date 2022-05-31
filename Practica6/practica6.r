89a996d9712fa5095ab047cad7da2b656f8708fe
facundo2gonzalez2 <facundo2gonzalez2@gmail.com>
2022-05-31 12:06:12 -0300

    practica 6

--- Practica6/practica6.r
+++ Practica6/practica6.r
@@ -130,3 +130,168 @@ ingresos
 ingresoMinimo=min(ingresos$V1)
 proba_ingresoMinimo=sum(ingresos$V1==ingresoMinimo)/1000
 proba_ingresoMinimo
+
+
+#b
+quantile(ingresos$V1,0.9)
+
+#c
+mean(ingresos$V1)
+median(ingresos$V1)
+mean(ingresos$V1,0.1)
+
+#d
+sd(ingresos$V1)
+IQR(ingresos$V1)
+
+#e
+
+hist(ingresos$V1)
+boxplot(ingresos$V1)
+#hay muchos outliers
+
+#f
+#si algo no es es una distribucion normal
+
+
+
+
+####ejercicio 6####
+# (descomentar de a una linea, comentando la otra para plotear diferentes funciones)
+
+#Muestras de 25,50 y 100 de una variable con dist normal
+set.seed(1001001)
+for (n in c(25,50,100)){
+  ej6muestra <- rnorm(n); nombre <- "Normal 0, 1"
+  #ej6muestra <- rgamma(n,scale=5, shape=1/2); nombre <- "Gamma 5, 1/2)"
+  #ej6muestra <- rnorm(n)/runif(n,min=0,max=1); nombre <- "Normal 0, 1 dividido Uniforme 0, 1"
+  #ej6muestra <- runif(n,min=0,max=1); nombre <- "Uniforme 0, 1"
+  #ej6muestra <- rexp(n); nombre <- "Exponencial 1"
+  
+  qqnorm(ej6muestra, main=nombre)
+}
+
+#se distingue facilmente entre una variable normal y una que no lo es
+
+
+
+
+
+
+
+####ejercicio 7####
+#7.a
+n <- 1000
+vector <- runif(n)
+l2 <- function(c) {
+  res <- 0
+  for(val in vector) {
+    res <- res + (val-c)**2
+  }
+  return(res)
+}
+x <- seq(-500, 500, by=1)
+plot(x, l2(x))
+
+#la función se minimiza en 0
+#con distintos valores de n sigue siguendo una cuadrática centrada en 0
+#sin embargo, si obtengo mi vector de una distribución uniforme muy grande (100000000), aparece una recta en lugar de una cuadrática.
+
+#falta demostrarlo
+
+
+#7.b
+l1 <- function(c) {
+  res <- 0
+  for(val in vector) {
+    res <- res + abs(val-c)
+  }
+  return(res)
+}
+plot(x, l1(x))
+
+#nuevamente la funci�n se minimiza en 0, y es un m�dulo
+
+
+
+
+
+
+
+
+####ejercicio 8####
+
+#a
+vectorAl <- rnorm(n)
+x1=mean(vectorAl)
+x2=median(vectorAl)
+vectorAux = c(vectorAl+5)
+y1=mean(vectorAux)
+y2=median(vectorAux)
+#y1,x1 y y2,x2 se diferencian en la constante agregada 5
+
+vectorAux2 = c(vectorAl*5)
+y11=mean(vectorAux2)
+y22=median(vectorAux2)
+x1*5==y11 #da false porque debe variar en algun decimal
+x2*5==y22 #devuelve true
+#x1 *5 = y11 y x2*5  =  y22
+
+#b
+vectorFuncX=rnorm(100)
+vectorFuncY=c(vectorFuncX+10)
+
+
+s2x=function(x){
+  res=0
+  for (val in vectorFuncX) {
+    res=res+ (val-mean(vectorFuncX))**2
+  }
+  return= (1/99)*res
+}
+
+s2y=function(y){
+  res=0
+  for (val in vectorFuncY) {
+    res=res+ (val-mean(vectorFuncY))**2
+  }
+  return= (1/99)*res
+}
+
+
+varianzaDeX=s2x(100)
+varianzaDeY=s2y(100)
+varianzaDeX
+varianzaDeY
+#efectivamente son iguales para Yi=Xi+10
+#falta demostraci�n formal
+
+vectorFuncY2=c(vectorFuncX*10)
+s2y2=function(y){
+  res=0
+  for (val in vectorFuncY2) {
+    res=res+ (val-mean(vectorFuncY2))**2
+  }
+  return= (1/99)*res
+}
+varianzaDeY2=s2y2(100)
+varianzaDeY2
+10**2*varianzaDeX
+#efectivamente son iguales para Yi=Xi*10
+
+
+s2x2=function(x){
+  res=0
+  for (val in vectorFuncX) {
+    res=res+ (val)**2
+  }
+  return= (1/99)*res- (100/99)*((mean(vectorFuncX))**2)
+}
+varianzaDeX2=s2x2(100)
+varianzaDeX2
+varianzaDeX
+#efectivamente valen lo mismo
+
+
+
+
---
+++ Practica7/practica7.R
@@ -0,0 +1,10 @@
+####ejercicio 1####
+carbohidratos=c(76.93, 76.88, 77.07, 76.68, 76.39, 75.09, 77.67, 76.88, 78.15, 76.50, 77.16, 76.42)
+
+esperanza=mean(carbohidratos)
+esperanza
+
+mediana=median(carbohidratos)
+mediana
+proba=sum(carbohidratos<76.5)/length(carbohidratos)
+proba
